16386ec217cd28f05d7882c2e198fd9cda1eef11
dougui <douglaspo_97@outlook.com>
2024-03-14 19:48:45 -0300

    feat: create posts component & latest posts section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import { Hero } from "@/components/Hero"
+import { Posts } from "@/components/Posts"
 
 export default function Home() {
   return (
     <main>
       <Hero />
+
+      <section className="mt-24 max-w-[1200px] flex flex-col items-center">
+        <h2 className="text-2xl font-bold">Latest Post</h2>
+
+        <Posts />
+
+        <button className="px-4 py-3 border-2 border-secondary_100 rounded-md text-[#696A75] mt-8 w-fit">
+          View All Post
+        </button>
+      </section>
     </main>
   )
 }
---
+++ src/components/Posts.tsx
@@ -0,0 +1,213 @@
+import Image from "next/image"
+import { PostCategory } from "./PostCategory"
+
+export function Posts() {
+  return (
+    <div className="grid grid-cols-3 gap-5 mt-8">
+      <div className="border-2 border-secondary_100 p-4 rounded-xl">
+        <Image
+          src="https://www.zoomtecnologia.com.br/wp-content/uploads/2020/12/thumbnail-datacenter-inteligente-huawei-fusionmodule.jpeg"
+          alt=""
+          width={360}
+          height={240}
+          className="rounded-md"
+        />
+
+        <div className="flex flex-col p-2 mt-4">
+          <PostCategory />
+
+          <strong className="text-2xl text-secondary_800 font-semibold mt-4">
+            The Impact of Technology on the Workplace: How Technology is
+            Changing
+          </strong>
+
+          <div className="flex items-center gap-5 mt-5">
+            <div className="flex gap-3 items-center">
+              <Image
+                src="https://www.jamsadr.com/images/neutrals/person-donald-900x1080.jpg"
+                alt=""
+                width={0}
+                height={0}
+                className="rounded-full w-9 h-9"
+              />
+              <strong className="font-medium text-secondary_400 font-work_sans">
+                Jason Francisco
+              </strong>
+            </div>
+            <p className="text-secondary_400 font-normal">August 20, 2022</p>
+          </div>
+        </div>
+      </div>
+      <div className="border-2 border-secondary_100 p-4 rounded-xl">
+        <Image
+          src="https://www.zoomtecnologia.com.br/wp-content/uploads/2020/12/thumbnail-datacenter-inteligente-huawei-fusionmodule.jpeg"
+          alt=""
+          width={360}
+          height={240}
+          className="rounded-md"
+        />
+
+        <div className="flex flex-col p-2 mt-4">
+          <PostCategory />
+
+          <strong className="text-2xl text-secondary_800 font-semibold mt-4">
+            The Impact of Technology on the Workplace: How Technology is
+            Changing
+          </strong>
+
+          <div className="flex items-center gap-5 mt-5">
+            <div className="flex gap-3 items-center">
+              <Image
+                src="https://www.jamsadr.com/images/neutrals/person-donald-900x1080.jpg"
+                alt=""
+                width={0}
+                height={0}
+                className="rounded-full w-9 h-9"
+              />
+              <strong className="font-medium text-secondary_400 font-work_sans">
+                Jason Francisco
+              </strong>
+            </div>
+            <p className="text-secondary_400 font-normal">August 20, 2022</p>
+          </div>
+        </div>
+      </div>
+      <div className="border-2 border-secondary_100 p-4 rounded-xl">
+        <Image
+          src="https://www.zoomtecnologia.com.br/wp-content/uploads/2020/12/thumbnail-datacenter-inteligente-huawei-fusionmodule.jpeg"
+          alt=""
+          width={360}
+          height={240}
+          className="rounded-md"
+        />
+
+        <div className="flex flex-col p-2 mt-4">
+          <PostCategory />
+
+          <strong className="text-2xl text-secondary_800 font-semibold mt-4">
+            The Impact of Technology on the Workplace: How Technology is
+            Changing
+          </strong>
+
+          <div className="flex items-center gap-5 mt-5">
+            <div className="flex gap-3 items-center">
+              <Image
+                src="https://www.jamsadr.com/images/neutrals/person-donald-900x1080.jpg"
+                alt=""
+                width={0}
+                height={0}
+                className="rounded-full w-9 h-9"
+              />
+              <strong className="font-medium text-secondary_400 font-work_sans">
+                Jason Francisco
+              </strong>
+            </div>
+            <p className="text-secondary_400 font-normal">August 20, 2022</p>
+          </div>
+        </div>
+      </div>
+      <div className="border-2 border-secondary_100 p-4 rounded-xl">
+        <Image
+          src="https://www.zoomtecnologia.com.br/wp-content/uploads/2020/12/thumbnail-datacenter-inteligente-huawei-fusionmodule.jpeg"
+          alt=""
+          width={360}
+          height={240}
+          className="rounded-md"
+        />
+
+        <div className="flex flex-col p-2 mt-4">
+          <PostCategory />
+
+          <strong className="text-2xl text-secondary_800 font-semibold mt-4">
+            The Impact of Technology on the Workplace: How Technology is
+            Changing
+          </strong>
+
+          <div className="flex items-center gap-5 mt-5">
+            <div className="flex gap-3 items-center">
+              <Image
+                src="https://www.jamsadr.com/images/neutrals/person-donald-900x1080.jpg"
+                alt=""
+                width={0}
+                height={0}
+                className="rounded-full w-9 h-9"
+              />
+              <strong className="font-medium text-secondary_400 font-work_sans">
+                Jason Francisco
+              </strong>
+            </div>
+            <p className="text-secondary_400 font-normal">August 20, 2022</p>
+          </div>
+        </div>
+      </div>
+      <div className="border-2 border-secondary_100 p-4 rounded-xl">
+        <Image
+          src="https://www.zoomtecnologia.com.br/wp-content/uploads/2020/12/thumbnail-datacenter-inteligente-huawei-fusionmodule.jpeg"
+          alt=""
+          width={360}
+          height={240}
+          className="rounded-md"
+        />
+
+        <div className="flex flex-col p-2 mt-4">
+          <PostCategory />
+
+          <strong className="text-2xl text-secondary_800 font-semibold mt-4">
+            The Impact of Technology on the Workplace: How Technology is
+            Changing
+          </strong>
+
+          <div className="flex items-center gap-5 mt-5">
+            <div className="flex gap-3 items-center">
+              <Image
+                src="https://www.jamsadr.com/images/neutrals/person-donald-900x1080.jpg"
+                alt=""
+                width={0}
+                height={0}
+                className="rounded-full w-9 h-9"
+              />
+              <strong className="font-medium text-secondary_400 font-work_sans">
+                Jason Francisco
+              </strong>
+            </div>
+            <p className="text-secondary_400 font-normal">August 20, 2022</p>
+          </div>
+        </div>
+      </div>
+      <div className="border-2 border-secondary_100 p-4 rounded-xl">
+        <Image
+          src="https://www.zoomtecnologia.com.br/wp-content/uploads/2020/12/thumbnail-datacenter-inteligente-huawei-fusionmodule.jpeg"
+          alt=""
+          width={360}
+          height={240}
+          className="rounded-md"
+        />
+
+        <div className="flex flex-col p-2 mt-4">
+          <PostCategory />
+
+          <strong className="text-2xl text-secondary_800 font-semibold mt-4">
+            The Impact of Technology on the Workplace: How Technology is
+            Changing
+          </strong>
+
+          <div className="flex items-center gap-5 mt-5">
+            <div className="flex gap-3 items-center">
+              <Image
+                src="https://www.jamsadr.com/images/neutrals/person-donald-900x1080.jpg"
+                alt=""
+                width={0}
+                height={0}
+                className="rounded-full w-9 h-9"
+              />
+              <strong className="font-medium text-secondary_400 font-work_sans">
+                Jason Francisco
+              </strong>
+            </div>
+            <p className="text-secondary_400 font-normal">August 20, 2022</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
